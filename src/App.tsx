ec618f3761061dd225f2e986fdfcce04f11c7f4a
Nchekwube1 <unekwenchekwube@gmail.com>
2021-11-13 05:53:52 +0100

    bundling and transpiling with unpkg and esbuild_wasm done

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import React, {useEffect,useState,useRef} from 'react';
+import './App.css';
+import * as esbuild_wasm from "esbuild-wasm"
+import {unpkgPathlugin} from "./plugis/unpkg_path"
+function App() {
+  const [input,setInput] = useState("")
+  const [code,setCode] = useState<string>("")
+  const ref = useRef<any>()
+  const startService = async()=>{
+  await esbuild_wasm.initialize({
+     worker:true,
+     wasmURL:"/esbuild.wasm"
+   })
+
+    ref.current = true
+  }
+  
+  useEffect(()=>{
+    startService()
+        },[])
+
+  const sub =  async(e:any)=>{
+
+    e.preventDefault()
+    if(!ref.current){
+      return
+    }
+    const target = await esbuild_wasm.build({
+     bundle:true,
+     write:false,
+     entryPoints:["index.js"],
+     plugins:[unpkgPathlugin()]
+   })
+
+   console.log(target)
+   setCode(target.outputFiles[0].text)
+   setInput("")
+    // console.log("submit",target)
+ }
+
+  return (
+    <div className="App">
+      <form onSubmit={sub}>
+        <textarea value={input} onChange={e=>setInput(e.target.value)} ></textarea>
+        <button type="submit">submit</button>
+      </form>
+
+      <h2>{code}</h2>
+    </div>
+  );
+}
+
+export default App;
